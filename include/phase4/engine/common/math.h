20a62e5697a03ee91d4ee4f2d59ff39f3f7d39b3
mashumafi <mashumafi@gmail.com>
2024-01-15 03:40:41 +0000

    lint

--- include/phase4/engine/common/math.h
+++ include/phase4/engine/common/math.h
@@ -1,3 +1,6 @@
+#ifndef PHASE4_ENGINE_COMMON_MATH_H
+#define PHASE4_ENGINE_COMMON_MATH_H
+
 #include <climits>
 #include <cstdint>
 
@@ -8,21 +11,23 @@ namespace phase4::engine::common {
 class Math {
 public:
 	static constexpr int16_t abs_int16(int16_t x);
-    static constexpr int16_t min_int16(int16_t x, int16_t y);
-    static constexpr int16_t max_int16(int16_t x, int16_t y);
+	static constexpr int16_t min_int16(int16_t x, int16_t y);
+	static constexpr int16_t max_int16(int16_t x, int16_t y);
 };
 
 constexpr int16_t Math::abs_int16(int16_t x) {
-	int16_t mask = x >> (sizeof(int16_t) * CHAR_BIT - 1);
+	const int16_t mask = x >> (sizeof(int16_t) * CHAR_BIT - 1);
 	return (x + mask) ^ mask;
 }
 
 constexpr int16_t Math::min_int16(int16_t x, int16_t y) {
-    return y ^ ((x ^ y) & -(x < y));
+	return y ^ ((x ^ y) & -(x < y));
 }
 
 constexpr int16_t Math::max_int16(int16_t x, int16_t y) {
-    return x ^ ((x ^ y) & -(x < y));
+	return x ^ ((x ^ y) & -(x < y));
 }
 
 } //namespace phase4::engine::common
+
+#endif
--- tests/phase4/engine/common/math.t.cpp
+++ tests/phase4/engine/common/math.t.cpp
@@ -3,23 +3,23 @@
 #include <doctest/doctest.h>
 
 TEST_CASE("Math abs") {
-    using namespace phase4::engine::common;
+	using namespace phase4::engine::common;
 
-    CHECK(Math::abs_int16(0) == 0);
-    CHECK(Math::abs_int16(-3) == 3);
-    CHECK(Math::abs_int16(6) == 6);
+	CHECK(Math::abs_int16(0) == 0);
+	CHECK(Math::abs_int16(-3) == 3);
+	CHECK(Math::abs_int16(6) == 6);
 }
 
 TEST_CASE("Math max") {
-    using namespace phase4::engine::common;
+	using namespace phase4::engine::common;
 
-    CHECK(Math::max_int16(0, 0) == 0);
-    CHECK(Math::max_int16(1, -3) == 1);
-    CHECK(Math::max_int16(-2, 4) == 4);
-    CHECK(Math::max_int16(-3, -9) == -3);
-    CHECK(Math::max_int16(3, 4) == 4);
-    CHECK(Math::max_int16(0, 4) == 4);
-    CHECK(Math::max_int16(5, 0) == 5);
-    CHECK(Math::max_int16(0, -8) == 0);
-    CHECK(Math::max_int16(-7, 0) == 0);
+	CHECK(Math::max_int16(0, 0) == 0);
+	CHECK(Math::max_int16(1, -3) == 1);
+	CHECK(Math::max_int16(-2, 4) == 4);
+	CHECK(Math::max_int16(-3, -9) == -3);
+	CHECK(Math::max_int16(3, 4) == 4);
+	CHECK(Math::max_int16(0, 4) == 4);
+	CHECK(Math::max_int16(5, 0) == 5);
+	CHECK(Math::max_int16(0, -8) == 0);
+	CHECK(Math::max_int16(-7, 0) == 0);
 }
