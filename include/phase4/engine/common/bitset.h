8ae99537934f045c362005d1b4e6912a063dbd7b
mashumafi <mashumafi@gmail.com>
2024-01-23 12:20:07 +0000

    __popcnt64

--- include/phase4/engine/common/bitset.h
+++ include/phase4/engine/common/bitset.h
@@ -162,7 +162,7 @@ inline constexpr Bitset Bitset::MAX(0b11111111'11111111'11111111'11111111'111111
 	return static_cast<std::size_t>(__builtin_popcountll(m_bits));
 #elif defined(_MSC_VER)
 	// Microsoft Visual C++
-	return static_cast<std::size_t>(_mm_popcnt_u64(m_bits));
+	return static_cast<std::size_t>(__popcnt64(m_bits));
 #else
 	// Fallback implementation for other compilers or platforms
 	uint8_t count = 0;
