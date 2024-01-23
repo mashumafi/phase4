9d54f5f47c657636e4f77351db21b7d354c23a37
mashumafi <mashumafi@gmail.com>
2024-01-23 12:13:38 +0000

    typo

--- include/phase4/engine/common/bitset.h
+++ include/phase4/engine/common/bitset.h
@@ -159,14 +159,10 @@ inline constexpr Bitset Bitset::MAX(0b11111111'11111111'11111111'11111111'111111
 [[nodiscard]] inline constexpr uint8_t Bitset::count() const noexcept {
 #if defined(__GNUC__) || defined(__clang__)
 	// GCC or Clang
-	constexpr std::size_t count_set_bits(uint64_t value) noexcept {
-		return static_cast<std::size_t>(__builtin_popcountll(value));
-	}
+	return static_cast<std::size_t>(__builtin_popcountll(value));
 #elif defined(_MSC_VER)
 	// Microsoft Visual C++
-	constexpr std::size_t count_set_bits(uint64_t value) noexcept {
-		return static_cast<std::size_t>(_mm_popcnt_u64(value));
-	}
+	return static_cast<std::size_t>(_mm_popcnt_u64(value));
 #else
 	// Fallback implementation for other compilers or platforms
 	uint8_t count = 0;
