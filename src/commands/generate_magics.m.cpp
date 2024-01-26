eab865de5dc7d493582f4f93a1ab079f2a3daddf
mashumafi <mashumafi@gmail.com>
2024-01-26 03:46:00 +0000

    logs

--- src/commands/generate_magics.m.cpp
+++ src/commands/generate_magics.m.cpp
@@ -8,6 +8,8 @@ int main() {
 	MagicBitboards::BishopMagicContainers bishopAttacks;
 	MagicBitboards::generateBishopAttacks(bishopAttacks);
 	if (!bishopAttacks.isValid) {
+		std::cout << "Could not generate bishop magics" << std::endl;
+		return 1;
 	}
 	std::cout << "Bishop magics:" << std::endl;
 	for (size_t i = 0; i < bishopAttacks.containers.size(); ++i) {
@@ -18,8 +20,12 @@ int main() {
 	MagicBitboards::RookMagicContainers rookAttacks;
 	MagicBitboards::generateRookAttacks(rookAttacks);
 	if (!rookAttacks.isValid) {
+		std::cout << "Could not generate rook magics" << std::endl;
+		return 1;
 	}
 	for (size_t i = 0; i < rookAttacks.containers.size(); ++i) {
 		std::cout << rookAttacks.containers[i].magicNumber.asSize() << "ULL," << std::endl;
 	}
+
+	return 0;
 }
