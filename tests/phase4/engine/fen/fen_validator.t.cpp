48c1293c519df3471811c84563b2ff028dd4c2cb
mashumafi <mashumafi@gmail.com>
2024-02-05 00:33:51 +0000

    tests

--- tests/phase4/engine/fen/fen_validator.t.cpp
+++ tests/phase4/engine/fen/fen_validator.t.cpp
@@ -40,11 +40,11 @@ TEST_CASE("FenValidator") {
 		FenTestCase{
 				"8/r4pk1/3Np3/p1PbP1pp/P7/7P/2R3P1/7K b - - 0 39",
 				"a7c7", { "d6e8", "g7f8", "e8c7" },
-				11, fen::FenValidator::FOUND_BEST },
+				19, fen::FenValidator::MOVES_MATCH },
 		FenTestCase{
 				"8/1p1k4/p1pp4/7R/PP2n1p1/4P1K1/2P5/8 w - - 0 38",
 				"g3g4", { "e4f6", "g4f5", "f6h5" },
-				12, fen::FenValidator::FOUND_BEST },
+				4, fen::FenValidator::FOUND_BEST },
 	};
 
 	for (const FenTestCase &testCase : testCases) {
