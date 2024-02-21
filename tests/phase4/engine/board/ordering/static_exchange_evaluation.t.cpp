b916a35d7c84d61b96ca068eaa2d1f4f63c0a7fc
mashumafi <mashumafi@gmail.com>
2024-02-21 03:15:17 +0000

    remove tests

--- tests/phase4/engine/board/ordering/static_exchange_evaluation.t.cpp
+++ tests/phase4/engine/board/ordering/static_exchange_evaluation.t.cpp
@@ -51,7 +51,7 @@ int32_t evaluate(const phase4::engine::board::Position &position, phase4::engine
 }
 } //namespace
 
-TEST_CASE("StaticExchangeEvaluation probing") {
+TEST_CASE("StaticExchangeEvaluation evaluate") {
 	using namespace phase4::engine;
 	using SEE = board::ordering::StaticExchangeEvaluation;
 
@@ -68,91 +68,6 @@ TEST_CASE("StaticExchangeEvaluation probing") {
 	CHECK(SEE::evaluate(common::PieceType::ROOK, common::PieceType::BISHOP, 0b01010101, 0b01010101) == -100);
 	CHECK(SEE::evaluate(common::PieceType::QUEEN, common::PieceType::BISHOP, 0b01010101, 0b01010101) == -720);
 	CHECK(SEE::evaluate(common::PieceType::KING, common::PieceType::BISHOP, 0b01010101, 0b01010101) == -19530);
-}
-
-TEST_CASE("StaticExchangeEvaluation computeResult") {
-	using namespace phase4::engine;
-
-	const auto position = fen::FenToPosition::parse("r2q1rk1/1b3pb1/1p2p1pB/p2p4/n7/5N1P/BPPQ1PP1/2KR3R w - - 0 19");
-	REQUIRE(position);
-
-	const std::optional<moves::Move> &realMove = board::PositionMoves::findRealMove(*position, moves::Move("d2d5"));
-	REQUIRE(realMove);
-	CHECK(realMove->flags() == moves::MoveFlags::CAPTURE); // Only testing captures
-
-	const common::PieceColor enemyColor = position->m_colorToMove.invert();
-
-	const common::PieceType attackingPiece = position->m_pieceTable[realMove->from()];
-	CHECK(attackingPiece == common::PieceType::QUEEN);
-	const common::PieceType capturedPiece = position->m_pieceTable[realMove->to()];
-	CHECK(capturedPiece == common::PieceType::PAWN);
-
-	common::Bitset attackers = board::ordering::SeePiece::getAttackingPiecesWithColor(*position, position->m_colorToMove, realMove->to());
-	common::Bitset defenders = board::ordering::SeePiece::getAttackingPiecesWithColor(*position, enemyColor, realMove->to());
-
-	CHECK(attackers == 0b01011000);
-	CHECK(defenders == 0b01001001);
-
-	attackers &= 0b10111111; // Remove queen as an attacker
-
-	common::SafeVector<int16_t, 7> gainList = {};
-
-	common::PieceType currentPieceOnField = attackingPiece;
-	int16_t result = 0;
-
-	gainList.push_back(result);
-
-	{
-		REQUIRE(defenders != 0);
-		common::PieceType leastValuableDefenderPiece = board::ordering::StaticExchangeEvaluation::getLeastValuablePiece(defenders);
-		CHECK(leastValuableDefenderPiece == common::PieceType::PAWN);
-		defenders = defenders.popLsb();
-
-		result -= board::EvaluationConstants::pieceValue(currentPieceOnField);
-		currentPieceOnField = leastValuableDefenderPiece;
-
-		gainList.push_back(result);
-	}
-
-	// while
-	{
-		REQUIRE(attackers != 0);
-		const common::PieceType leastValuableAttackerPiece = board::ordering::StaticExchangeEvaluation::getLeastValuablePiece(attackers);
-		CHECK(leastValuableAttackerPiece == common::PieceType::BISHOP);
-		attackers = attackers.popLsb();
-
-		result += board::EvaluationConstants::pieceValue(currentPieceOnField);
-		currentPieceOnField = leastValuableAttackerPiece;
-
-		gainList.push_back(result);
-
-		CHECK(gainList.peek() <= gainList.peek(2));
-	}
-
-	{
-		REQUIRE(defenders != 0);
-
-		const common::PieceType leastValuableDefenderPiece = board::ordering::StaticExchangeEvaluation::getLeastValuablePiece(defenders);
-		CHECK(leastValuableDefenderPiece == common::PieceType::BISHOP);
-		defenders = defenders.popLsb();
-
-		result -= board::EvaluationConstants::pieceValue(currentPieceOnField);
-		currentPieceOnField = leastValuableDefenderPiece;
-
-		gainList.push_back(result);
-
-		CHECK(gainList.peek() < gainList.peek(2));
-		result = gainList.peek(2);
-	}
-
-	CHECK(attackers == 0b00010000); // White Rook
-	CHECK(defenders == 0b01000000); // Black Queen
-
-	result += board::EvaluationConstants::pieceValue(capturedPiece);
-
-	const int32_t blackSee = Q;
-	const int32_t whiteSee = P;
-	CHECK(result == whiteSee - blackSee);
 }
 
 TEST_CASE("StaticExchangeEvaluation populate") {
@@ -217,36 +132,6 @@ TEST_CASE("StaticExchangeEvaluation populate") {
 		CHECK(kingTable[0b00010101][0b01010101] == -19900);
 		CHECK(kingTable[0b01010101][0b00010101] == -19900);
 	}
-}
-
-TEST_CASE("StaticExchangeEvaluation evaluate") {
-	using namespace phase4::engine;
-
-	const auto position = fen::FenToPosition::parse("8/pp3pk1/2p3Pr/4q3/8/6RP/PPQ2PRK/4r3 b - - 0 26");
-	REQUIRE(position);
-	CHECK(position->m_colorToMove == common::PieceColor::BLACK);
-
-	const std::optional<moves::Move> &realMove = board::PositionMoves::findRealMove(*position, moves::Move("g7g6"));
-	REQUIRE(realMove);
-	CHECK(realMove->flags() == moves::MoveFlags::CAPTURE); // Only testing captures
-
-	const common::PieceColor enemyColor = position->m_colorToMove.invert();
-	CHECK(enemyColor == common::PieceColor::WHITE);
-
-	const common::PieceType attackingPiece = position->m_pieceTable[realMove->from()];
-	const common::PieceType capturedPiece = position->m_pieceTable[realMove->to()];
-	CHECK(attackingPiece == common::PieceType::KING);
-	CHECK(capturedPiece == common::PieceType::PAWN);
-
-	const uint8_t attackers = board::ordering::SeePiece::getAttackingPiecesWithColor(*position, position->m_colorToMove, realMove->to());
-	const uint8_t defenders = board::ordering::SeePiece::getAttackingPiecesWithColor(*position, enemyColor, realMove->to());
-	CHECK(attackers == 0b10010001);
-	CHECK(defenders == 0b1110000);
-	const int32_t seeEvaluation = board::ordering::StaticExchangeEvaluation::evaluate(attackingPiece, capturedPiece, attackers, defenders);
-
-	const int32_t blackSee = K;
-	const int32_t whiteSee = R + P;
-	CHECK(seeEvaluation == whiteSee - blackSee);
 }
 
 TEST_CASE("StaticExchangeEvaluation -19330 g7g6 8/pp3pk1/2p3Pr/4q3/8/6RP/PPQ2PRK/4r3 b - - 0 26") {
